fe211833eb9e497e978269951ea50b25bf6b7737
lakshay-deep <dlakshay20@gmail.com>
2024-06-20 00:37:38 +0530

    AddOnBoardingScreen

--- app/src/main/java/com/example/newz/MainActivity.kt
+++ app/src/main/java/com/example/newz/MainActivity.kt
@@ -3,7 +3,12 @@ package com.example.newz
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.ui.Modifier
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import com.example.newz.presentation.onboarding.OnBoardingScreen
 import com.example.newz.ui.theme.NewsAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -11,7 +16,12 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         installSplashScreen()
         setContent {
-            NewsAppTheme {
+            NewsAppTheme(
+                dynamicColor = false
+            ) {
+                Box(modifier = Modifier.background(MaterialTheme.colorScheme.background)) {
+                    OnBoardingScreen()
+                }
             }
         }
     }
