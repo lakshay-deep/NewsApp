03c0b5b793ecf29f298ba9eaf38faec76ba5ba9d
lakshay-deep <dlakshay20@gmail.com>
2024-06-21 17:38:11 +0530

    NavGraph

--- app/src/main/java/com/example/newz/presentation/mainactivity/MainActivity.kt
+++ app/src/main/java/com/example/newz/presentation/mainactivity/MainActivity.kt
@@ -1,8 +1,9 @@
-package com.example.newz
+package com.example.newz.presentation.mainactivity
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.viewModels
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.material3.MaterialTheme
@@ -10,6 +11,7 @@ import androidx.compose.ui.Modifier
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.core.view.WindowCompat
 import androidx.hilt.navigation.compose.hiltViewModel
+import com.example.newz.presentation.navgraph.NavGraph
 import com.example.newz.presentation.onboarding.OnBoardingScreen
 import com.example.newz.presentation.onboarding.OnBoardingViewModel
 import com.example.newz.ui.theme.NewsAppTheme
@@ -17,17 +19,20 @@ import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+
+    private val viewModel by viewModels<MainViewModel>()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         WindowCompat.setDecorFitsSystemWindows(window, false)
-        installSplashScreen()
+        installSplashScreen().apply {
+            setKeepOnScreenCondition(condition = { viewModel.splashCondition.value})
+        }
         setContent {
             NewsAppTheme(
                 dynamicColor = false
             ) {
                 Box(modifier = Modifier.background(MaterialTheme.colorScheme.background)) {
-                    val viewModel: OnBoardingViewModel = hiltViewModel()
-                    OnBoardingScreen(onEvent = viewModel::onEvent)
+                    NavGraph(startDestination = viewModel.startDestination.value)
                 }
             }
         }
